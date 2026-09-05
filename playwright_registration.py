f9da1ed54d5e5ff038452895c26b80183bda89b8
Lidiya <49871988+lidiyaAkv@users.noreply.github.com>
2026-09-05 17:14:11 +0300

    Введение в Playwright. Практика с Playwright

--- playwright_registration.py
+++ playwright_registration.py
@@ -6,23 +6,22 @@ with sync_playwright() as playwright:
 
     page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration")
 
-    registration_form_email_input = page.get_by_test_id('registration-form-email-input').locator('input')
-    expect(registration_form_email_input).to_be_visible()
-    registration_form_email_input.fill("user.name@gmail.com")
+    email_input = page.get_by_test_id('registration-form-email-input').locator('input')
+    expect(email_input).to_be_visible()
+    email_input.fill("user.name@gmail.com")
 
-    registration_form_username_input = page.get_by_test_id('registration-form-username-input').locator('input')
-    expect(registration_form_username_input).to_be_visible()
-    registration_form_username_input.fill("username")
+    username_input = page.get_by_test_id('registration-form-username-input').locator('input')
+    expect(username_input).to_be_visible()
+    username_input.fill("username")
 
-    registration_form_password_input = page.get_by_test_id('registration-form-password-input').locator('input')
-    expect(registration_form_password_input).to_be_visible()
-    registration_form_password_input.fill("password")
+    password_input = page.get_by_test_id('registration-form-password-input').locator('input')
+    expect(password_input).to_be_visible()
+    password_input.fill("password")
 
-    registration_page_registration_button = page.get_by_test_id('registration-page-registration-button')
-    expect(registration_page_registration_button).to_be_visible()
-    registration_page_registration_button.click()
+    registration_button = page.get_by_test_id('registration-page-registration-button')
+    expect(registration_button).to_be_visible()
+    registration_button.click()
 
-    dashboard_toolbar_title_text = page.get_by_test_id('dashboard-toolbar-title-text')
-    expect(dashboard_toolbar_title_text).to_be_visible()
-
-    page.wait_for_timeout(2500)
+    dashboard_title = page.get_by_test_id('dashboard-toolbar-title-text')
+    expect(dashboard_title).to_be_visible()
+    expect(dashboard_title).to_have_text('Dashboard')
