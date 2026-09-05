6fca63644530adeebab58f98ed084b61e91530c6
Lidiya <49871988+lidiyaAkv@users.noreply.github.com>
2026-09-05 13:54:30 +0300

    Введение в Playwright. Закрепление теории по Playwright updated

--- elements_on_login_and_registration_pages.py
+++ elements_on_login_and_registration_pages.py
@@ -18,13 +18,13 @@ with sync_playwright() as playwright:
     login_page_registration_link = page.get_by_test_id('login-page-registration-link')
     login_page_registration_link.click()
 
-    registration_form_email_input = page.get_by_test_id('registration-form-email-input').locator('input')
+    registration_form_email_input = page.locator('div[data-testid="registration-form-email-input"] input')
     expect(registration_form_email_input).to_be_visible()
 
-    registration_form_password_input = page.get_by_test_id('registration-form-password-input').locator('input')
+    registration_form_password_input = page.locator('div[data-testid="registration-form-password-input"] input')
     expect(registration_form_password_input).to_be_visible()
 
-    registration_page_registration_button = page.get_by_test_id('registration-page-registration-button')
+    registration_page_registration_button = page.locator('button#registration-page-registration-button')
     expect(registration_page_registration_button).to_be_visible()
 
     page.wait_for_timeout(2500)
