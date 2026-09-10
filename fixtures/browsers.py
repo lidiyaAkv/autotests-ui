5cd6de893121c134b4edfcf2e1c3f44dae2c27a4
Lidiya <49871988+lidiyaAkv@users.noreply.github.com>
2026-09-10 12:13:16 +0300

    Плагины Pytest - 2

--- fixtures/browsers.py
+++ fixtures/browsers.py
@@ -22,14 +22,17 @@ def initialize_browser_state(playwright: Playwright) -> None:
     registration_button.click()
 
     context.storage_state(path='browser-state.json')
+    browser.close()
 
 @pytest.fixture
 def chromium_page_with_state(initialize_browser_state, playwright: Playwright) -> Page:
     browser = playwright.chromium.launch(headless=False)
     context = browser.new_context(storage_state='browser-state.json')
     yield context.new_page()
+    browser.close()
 
 @pytest.fixture
 def chromium_page(playwright: Playwright) -> Page:
     browser = playwright.chromium.launch(headless=False)
     yield browser.new_page()
+    browser.close()
