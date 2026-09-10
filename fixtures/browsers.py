4bf2635e49fa95f7f5fa1ac879be05a74a01faaf
Lidiya <49871988+lidiyaAkv@users.noreply.github.com>
2026-09-10 11:41:15 +0300

    Фикстуры Pytest. Практика работы с pytest фикстурами

--- fixtures/browsers.py
+++ fixtures/browsers.py
@@ -22,15 +22,17 @@ def initialize_browser_state(playwright: Playwright) -> None:
     registration_button.click()
 
     context.storage_state(path='browser-state.json')
+    browser.close()
 
 @pytest.fixture
 def chromium_page_with_state(initialize_browser_state, playwright: Playwright) -> Page:
     browser = playwright.chromium.launch(headless=False)
     context = browser.new_context(storage_state='browser-state.json')
     yield context.new_page()
+    browser.close()
 
 @pytest.fixture
 def chromium_page(playwright: Playwright) -> Page:
     browser = playwright.chromium.launch(headless=False)
     yield browser.new_page()
-    browser.close()  # это для наглядности, браузер будет и так закрываться при выходе из контекста
+    browser.close()
