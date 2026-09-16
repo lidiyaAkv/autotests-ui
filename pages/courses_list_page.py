262b35723fd81a83f92636cb1336e368990dee45
Lidiya <49871988+lidiyaAkv@users.noreply.github.com>
2026-09-16 13:03:48 +0300

    Знакомство с PageComponent. Практика работы с Page Component

--- pages/courses_list_page.py
+++ pages/courses_list_page.py
@@ -1,9 +1,14 @@
 from playwright.sync_api import Page, expect
 from pages.base_page import BasePage
+from components.navigation.navbar_component import NavBarComponent
+from components.navigation.sidebar_component import SidebarComponent
 
 class CoursesListPage(BasePage):
     def __init__(self, page: Page):
         super().__init__(page)
+
+        self.navbar = NavBarComponent(page)
+        self.sidebar = SidebarComponent(page)
 
         self.courses_title = page.get_by_test_id("courses-list-toolbar-title-text")
         self.create_course_button = page.get_by_test_id("courses-list-toolbar-create-course-button")
--- tests/test_dashboard.py
+++ tests/test_dashboard.py
@@ -8,6 +8,7 @@ def test_dashboard_displaying(dashboard_page_with_state: DashboardPage):
     dashboard_page_with_state.visit('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/dashboard')
 
     dashboard_page_with_state.navbar.check_visible(username='username')
+    dashboard_page_with_state.sidebar.check_visible()
 
     dashboard_page_with_state.check_dashboard_title()
 
