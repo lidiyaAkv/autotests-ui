1468cf8296d4942634f8186d97f482e25f247b7f
Lidiya <49871988+lidiyaAkv@users.noreply.github.com>
2026-09-17 15:15:06 +0300

    Практикуемся в реализации PageComponent. Практикуемся в написании компонентов с использованием паттерна PageComponent

--- tests/test_dashboard.py
+++ tests/test_dashboard.py
@@ -10,10 +10,10 @@ def test_dashboard_displaying(dashboard_page_with_state: DashboardPage):
     dashboard_page_with_state.navbar.check_visible(username='username')
     dashboard_page_with_state.sidebar.check_visible()
 
-    dashboard_page_with_state.check_dashboard_title()
+    dashboard_page_with_state.toolbar_view.check_visible()
 
-    dashboard_page_with_state.check_visible_students_chart()
     dashboard_page_with_state.check_visible_activities_chart()
+    dashboard_page_with_state.check_visible_students_chart()
     dashboard_page_with_state.check_visible_courses_chart()
     dashboard_page_with_state.check_visible_scores_chart()
 
