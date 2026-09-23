5c04f6f2f72ee7670689c235b29388dcf87201b3
Lidiya <49871988+lidiyaAkv@users.noreply.github.com>
2026-09-23 11:19:53 +0300

    Добавление Playwright Trace Viewer в Allure отчет

--- fixtures/browsers.py
+++ fixtures/browsers.py
@@ -1,8 +1,8 @@
 from playwright.sync_api import Page, Playwright
 import pytest
+from _pytest.fixtures import SubRequest
+import allure
 
-from elements.button import Button
-from elements.input import Input
 from pages.authentication.registration_page import RegistrationPage
 
 
@@ -20,15 +20,30 @@ def initialize_browser_state(playwright: Playwright) -> None:
     context.storage_state(path='browser-state.json')
     browser.close()
 
-@pytest.fixture
-def chromium_page_with_state(initialize_browser_state, playwright: Playwright) -> Page:
-    browser = playwright.chromium.launch(headless=False)
-    context = browser.new_context(storage_state='browser-state.json')
-    yield context.new_page()
-    browser.close()
 
 @pytest.fixture
-def chromium_page(playwright: Playwright) -> Page:
+def chromium_page_with_state(request: SubRequest, initialize_browser_state, playwright: Playwright) -> Page:
     browser = playwright.chromium.launch(headless=False)
-    yield browser.new_page()
+    context = browser.new_context(storage_state='browser-state.json')
+    context.tracing.start(screenshots=True, snapshots=True, sources=True)
+
+    yield context.new_page()
+
+    context.tracing.stop(path=f'./tracing/{request.node.name}.zip')
     browser.close()
+
+    allure.attach.file(source=f'./tracing/{request.node.name}.zip', name='trace', extension='zip')
+
+
+@pytest.fixture
+def chromium_page(request: SubRequest, playwright: Playwright) -> Page:
+    browser = playwright.chromium.launch(headless=False)
+    context = browser.new_context()
+    context.tracing.start(screenshots=True, snapshots=True, sources=True)
+
+    yield context.new_page()
+
+    context.tracing.stop(path=f'./tracing/{request.node.name}.zip')
+    browser.close()
+
+    allure.attach.file(source=f'./tracing/{request.node.name}.zip', name='trace', extension='zip')
