62eb17ae385c26880823ff5565d2f9602b0d1af2
Lidiya <49871988+lidiyaAkv@users.noreply.github.com>
2026-09-25 12:20:01 +0300

    Запуск автотестов на разных браузерах. Практическое задание: запуск автотестов на нескольких браузерах

--- tools/playwright/pages.py
+++ tools/playwright/pages.py
@@ -11,8 +11,8 @@ def initialize_playwright_page(
     browser = playwright[browser_type].launch(headless=settings.headless)
     context = browser.new_context(
         base_url=settings.get_base_url(),
-        storage_state=storage_state,
-        record_video_dir=settings.videos_dir
+        storage_state=storage_state
+        #record_video_dir=settings.videos_dir
     )
     #context.tracing.start(screenshots=True, snapshots=True, sources=True)
     page = context.new_page()
